4cafd378d4d934dfc6ff0134b9e564f24c7e668b
TJZYMR <tatva.joshi@zymr.com>
2022-06-30 11:11:17 +0530

    THread synchro

--- Thread_Synchronization.py
+++ Thread_Synchronization.py
@@ -1,18 +1,30 @@
-from threading import Thread, current_thread
+from threading import *
+import time
 class Flight:
     def __init__(self, available_seat):
         self.available_seat = available_seat
+        self.l=Lock()
     def reserve(self, need_seat):
+        # self.l.acquire(blocking=True,timeout=2)
+        self.l.acquire(blocking=False)
+
         print('Available Seats:', self.available_seat)
         if(self.available_seat >= need_seat):
             name = current_thread().name
             print(f'{need_seat} seat is allowed for {name}')
             self.available_seat -= need_seat
+            # time.sleep(3)
         else:
             print('Sorry! All seats has alloted')
-f = Flight(1)
+        self.l.release()
+f = Flight(2)
 t1 = Thread(target=f.reserve, args=(1,), name='Rahul')
 t2 = Thread(target=f.reserve, args=(1,), name='Soname')
-
-t2.start()
+t3 = Thread(target=f.reserve, args=(2,), name='raj')
 t1.start()
+t2.start()
+t3.start()
+t1.join()
+t2.join()
+t3.join()
+print("Main Thread")
